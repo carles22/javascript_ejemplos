f3774e98372861e8bd181129d021916f084c2dcf
carles22 <carlesconti@gmail.com>
2020-05-13 12:00:12 +0200

    modificacion archivo js

---
+++ js/basico.js
@@ -0,0 +1,40 @@
+//ejemplo de objeto
+
+{
+    "usuario": "Carles",
+    "departamento": "Contabilidad",
+    "horas_extra": 14,
+    "horas_devengadas": 2,
+    "consecucion_objetivos_mes": true
+}
+
+
+//ejemplo de variable
+
+var numero
+
+//definición de valor en variable
+
+var numero = 45;
+
+
+//ejemplo de array
+
+var array = [2,5,8,11,59,94,154]
+
+//ejemplo de definición de función
+
+function suma(valor1, valor2, valor3) {
+
+        console.log(valor1 + valor2 + valor3);
+
+}
+
+
+//ejemplo de llamada a función
+
+suma(2,5,78);
+
+//output del sistema al llamar a suma: 85
+
+
